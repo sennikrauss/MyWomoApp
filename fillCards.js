12dc828122e78471a5d52767e22c5ebf40bdc56f
PMahulome <s0569780@htw-berlin.de>
2022-09-07 19:42:25 +0200

    implemented createCard function

---
+++ fillCards.js
@@ -0,0 +1,22 @@
+function createCard(card) {
+  let divColLg6 = document.createElement('div');
+  divColLg6.className = 'col-lg-6';
+  sharedCardsArea.appendChild(divColLg6);
+
+  let divCardColumnsFluid = document.createElement('div');
+  divCardColumnsFluid.className = 'card-columns-fluid';
+  divColLg6.appendChild(divCardColumnsFluid);
+
+  let divCardBgLight = document.createElement('div');
+  divCardBgLight.className = 'card bg-light';
+  divCardBgLight.style.marginBottom = '1rem';
+  let image = new Image();
+  image.src = card.imageData;
+  image.className = "card-img-top";
+  image.style.width = "100%";
+  image.style.height = "15vw";
+  image.style.objectFit = "cover";
+  divCardBgLight.appendChild(image);
+  divCardColumnsFluid.appendChild(divCardBgLight);
+
+}
